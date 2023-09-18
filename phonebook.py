b8174973e3b9670f676594dbfb4bf3658a08c6a5
Dulina jayarathna <jayarathnadulina@gmail.com>
2023-09-18 18:48:50 +0530

    genaralize db connectio code

--- phonebook.py
+++ phonebook.py
@@ -1,5 +1,12 @@
 import mysql.connector
 
+# connect to database
+mydb = mysql.connector.connect(
+    host="localhost", user="root", password="Gshock@123", database="phonebook"
+)
+mycursor = mydb.cursor()
+
+
 print("1.Add new contact")
 print("2.view all contacts")
 print("3.delete contact")
@@ -11,12 +18,7 @@ if selected_option == 1:
     name = input("Enter name : ")
     phone = input("Enter contact number : ")
 
-    # connect to database
-    mydb = mysql.connector.connect(
-        host="localhost", user="root", password="Gshock@123", database="phonebook"
-    )
     # save to database
-    mycursor = mydb.cursor()
     insert_sql = "insert into contacts(name,phone) values(%s,%s)"
     data = (name, phone)
     mycursor.execute(insert_sql, data)
@@ -24,12 +26,6 @@ if selected_option == 1:
 
 
 elif selected_option == 2:
-    # connect to database
-    mydb = mysql.connector.connect(
-        host="localhost", user="root", password="Gshock@123", database="phonebook"
-    )
-    mycursor = mydb.cursor()
-
     # read all contacts from database
     mycursor.execute("select * from contacts")
     contacts = mycursor.fetchall()
@@ -41,12 +37,6 @@ elif selected_option == 2:
 elif selected_option == 3:
     # read the contact id to delete
     id = int(input("Enter contact id to delete : "))
-
-    # connect to database
-    mydb = mysql.connector.connect(
-        user="root", host="localhost", password="Gshock@123", database="phonebook"
-    )
-    mycursor = mydb.cursor()
 
     # create the query
     delete_query = "DELETE FROM contacts WHERE id=%s"
@@ -62,13 +52,7 @@ elif selected_option == 4:
 
     # read the new phone number to be modified
     new_number = input("Enter new phone number : ")
-
-    # connect to database
-    mydb = mysql.connector.connect(
-        user="root", host="localhost", password="Gshock@123", database="phonebook"
-    )
-    mycursor = mydb.cursor()
-
+       
     # build update query
     update_query = "update contacts set phone=%s where id = %s"
 
